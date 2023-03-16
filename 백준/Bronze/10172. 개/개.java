38668cf594c69baa021b51700eb8be9de66aa5dd
kcm02 <112393082+kcm02@users.noreply.github.com>
2023-03-16 22:08:01 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//    	Scanner sc = new Scanner(System.in);
+//		
+//        long a = sc.nextLong();
+//        long b = sc.nextLong();
+//        long c = sc.nextLong();
+//		
+//        System.out.println(a+b+c);
+    	
+    	System.out.println("|\\_/|");
+    	System.out.println("|q p|   /}");
+    	System.out.println("( 0 )\"\"\"\\");
+    	System.out.println("|\"^\"`    |");
+    	System.out.println("||_/=\\\\__|");
+	}
+}

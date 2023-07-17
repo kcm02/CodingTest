d4832e20cf8fe466a1029dcfa7cc8035f4e4310c
kcm02 <112393082+kcm02@users.noreply.github.com>
2023-07-17 12:42:35 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,15 +1,9 @@
 n,m = map(int,input().split())
-list = []
-ball = 0
-
-for i in range(n):
-    list.append(i+1)
+list = [i for i in range(1,n+1)]
 
 for i in range(m):
     i,j = map(int,input().split())
-    ball = list[i-1]
-    list[i-1] = list[j-1]
-    list[j-1] = ball
+    list[i-1],list[j-1] = list[j-1],list[i-1]
 
 for i in range(n):
     print(f'{list[i]} ',end="")

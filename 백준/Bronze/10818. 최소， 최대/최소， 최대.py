94aa67e8c21abc00581e060add4101b7f185aa2d
kcm02 <112393082+kcm02@users.noreply.github.com>
2023-07-17 12:35:09 +0900

    [Bronze III] Title: 최소, 최대, Time: 380 ms, Memory: 155728 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,13 +1,6 @@
 n = int(input())
 list = list(map(int,input().split()))
-max = list[0]
-min = list[0]
+min = min(list)
+max = max(list)
 
-for i in range(n):
-    if i>0:
-        if list[i] > max:
-            max = list[i]
-        elif list[i] < min:
-            min = list[i]
-
-print(f'{min} {max}')
+print(min,max)

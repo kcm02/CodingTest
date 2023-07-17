b7e45e9f3f62a6da1dc2644c3bcc06f09b181cf3
kcm02 <112393082+kcm02@users.noreply.github.com>
2023-07-17 11:24:50 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,16 @@
+student = set([])
+report = set({})
+
+for i in range(30):
+    student.add(i+1)
+
+for i in range(28):
+    report.add(int(input()))
+
+a,b = map(int,student - report)
+if a > b:
+    print(b)
+    print(a)
+else:
+    print(a)
+    print(b)

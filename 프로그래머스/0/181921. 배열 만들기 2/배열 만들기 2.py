b6ba98352b8ddd9d0167d19e5751193dc2c8fcf7
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-05 16:21:39 +0900

    [level 0] Title: 배열 만들기 2, Time: 21.82 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,9 +1,6 @@
 def solution(l, r):
     result = []
-    for i in range(l,r+1):
-        for j in str(i):
-            if j != "0" and j != "5":
-                break
-        else:
+    for i in range(l, r + 1):
+        if all(c in {'0', '5'} for c in str(i)):
             result.append(i)
-    return sorted(result) if result else [-1]
+    return result if result else [-1]

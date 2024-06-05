853c01c0d0acab9304da7fa89713e395d2502654
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-05 15:57:30 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 21.37 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,12 +1,9 @@
 def solution(arr, qrs):
     result = []
     for s, e, k in qrs:
-        l = []
+        tep = []
         for i in range(len(arr)):
             if s <= i <= e and arr[i] > k:
-                l.append(arr[i])
-        if l:
-            result.append(min(l))
-        else:
-            result.append(-1)
+                tep.append(arr[i])
+        result.append(min(tep) if tep else -1)
     return result

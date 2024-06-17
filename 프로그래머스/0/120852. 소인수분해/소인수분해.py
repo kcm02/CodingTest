07018b4a55c58c058dac215f758d3da39b285bcd
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-17 14:02:45 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = []
+    d = 2
+    while d <= n:
+        if n % d == 0:
+            n /= d
+            if d not in answer:
+                answer.append(d)
+        else:
+            d += 1
+    return answer

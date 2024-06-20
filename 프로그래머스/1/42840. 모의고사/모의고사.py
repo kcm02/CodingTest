a062cda3729a74d07ce281e8bd159e89269ab5f9
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-20 16:20:50 +0900

    [level 1] Title: 모의고사, Time: 6.13 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,19 @@
+def solution(answers):
+    patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
+    
+    count = [0] * len(patterns)
+    
+    for i, x in enumerate(answers):
+        for j in range(len(patterns)):
+            pattern = patterns[j]
+            if x == pattern[i % len(pattern)]:
+                count[j] += 1
+    
+    max_score = max(count)
+    highest_scores = [i+1 for i, x in enumerate(count) if x == max_score]
+            
+    return highest_scores

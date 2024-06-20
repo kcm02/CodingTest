5b64905edaf33cc5e8f2a708113efba42361a7f9
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-20 16:54:54 +0900

    [level 1] Title: 행렬의 덧셈, Time: 14.40 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,3 +1,3 @@
-def solution(arr1, arr2):
-    answer = [[arr1[i][j] + arr2[i][j] for j in range(len(arr1[i]))] for i in range(len(arr1))]
+def solution(A, B):
+    answer = [[c + d for c, d in zip(a,b)] for a, b in zip(A,B)]
     return answer

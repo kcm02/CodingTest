ae4635f4deb8f00faa4747310842cbabb02cc5f6
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-06-21 17:21:51 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,11 @@
+def solution(d, budget):
+    sorted_d = sorted(d)
+    count = 0
+    
+    for i in sorted_d:
+        if budget < i:
+            break
+        budget -= i
+        count += 1
+        
+    return count

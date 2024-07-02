b63b76cffa594f8b4472fab02059940a18bf88f7
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-07-02 17:05:58 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,21 +1,12 @@
 def solution(id_list, report, k):
-    report = list(set(report))
-    report_count = {user: 0 for user in id_list}
-    user_reports = {user: [] for user in id_list}
-    
-    for cmd in report:
-        reporter, reportee = cmd.split()
-        if reportee not in user_reports[reporter]:
-            user_reports[reporter].append(reportee)
-            report_count[reportee] += 1
-            
-    suspended_users = [user for user, count in report_count.items() if count >= k]
-    
-    mail_count = {user: 0 for user in id_list}
-    
-    for reporter in id_list:
-        for reportee in user_reports[reporter]:
-            if reportee in suspended_users:
-                mail_count[reporter] += 1
-                
-    return [mail_count[user] for user in id_list]
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer

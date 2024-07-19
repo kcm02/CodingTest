e16e4ce9421586ebfacb1f47297206714e9b92b7
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-07-19 14:35:37 +0900

    [level 1] Title: 문자열 나누기, Time: 1.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,23 +1,13 @@
 def solution(s):
-    count = 0
-    index = 0
-    
-    while index < len(s):
-        x = s[index]
-        same = 0
-        diff = 0
-        
-        while index < len(s):
-            if s[index] == x:
-                same += 1
-            else:
-                diff += 1
-            
-            if same == diff:
-                break
-            index += 1
-            
-        count += 1
-        index += 1
-    
-    return count
+    answer = 0
+    same=0
+    diff=0
+    for c in s:
+        if same == diff:
+            answer += 1
+            x = c
+        if c == x:
+            same += 1
+        else:
+            diff += 1
+    return answer

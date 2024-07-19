d4b4650d5dcbb81e62215b9f4fdf5f25d03af52d
kcm02 <112393082+kcm02@users.noreply.github.com>
2024-07-19 13:49:38 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -2,5 +2,5 @@ def solution(lottos, win_nums):
     grade = {6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6}
     if_num = lottos.count(0)
     worst = sum(1 for i in lottos if i in win_nums and i != 0)
-    best = worst + if_num
+    best = min(worst + if_num, 6)
     return [grade[best], grade[worst]]

dc9fc392d3a8eb9cb3eb9bb96fd55662702641b0
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-16 14:24:07 +0900

    [level 1] Title: 약수의 합, Time: 0.29 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,6 +1,2 @@
 def solution(n):
-    divs = []
-    for i in range(1, n+1):
-        if not n % i:
-            divs.append(i)
-    return sum(divs)
+    return sum(i for i in range(1,n+1) if not n % i)

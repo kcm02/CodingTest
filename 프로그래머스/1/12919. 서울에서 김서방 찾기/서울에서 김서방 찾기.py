26c783165a91f3208a865dee7f5a2a51808a20e2
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-16 14:45:05 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.77 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,3 +1,4 @@
 def solution(seoul):
-    x = seoul.index("Kim")
-    return f"김서방은 {x}에 있다"
+    # x = seoul.index("Kim")
+    # return f"김서방은 {x}에 있다"
+    return "김서방은 {}에 있다".format(seoul.index("Kim"))

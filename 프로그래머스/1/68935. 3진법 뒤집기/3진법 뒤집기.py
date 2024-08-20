2c87770c51cad784222fbf9a61977149d6a6b8d7
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-20 14:45:45 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,11 +1,8 @@
 def solution(n):
-    base_3 = ''
-    
+    s = ''
     while n > 0:
-        base_3 = str(n % 3) + base_3
-        n = n // 3
-        
-    for i,x in enumerate(base_3):
+        s = str(n % 3) + s
+        n //= 3
+    for i,x in enumerate(s):
         n += 3**i * int(x)
-        
     return n

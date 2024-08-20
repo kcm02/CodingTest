4107edaf6529c41b8cac3c1747144e37b9323212
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-20 15:46:39 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,5 +1,2 @@
 def solution(array, commands):
-    answer = []
-    for a,b,c in commands:
-        answer.append(sorted(array[a-1:b])[c-1])
-    return answer
+    return [sorted(array[a-1:b])[c-1] for a,b,c in commands]

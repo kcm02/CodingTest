b42a7edaa3fb54a25d0af2e5bedb6c670a2d7590
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-20 14:34:51 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,11 +1,9 @@
 def solution(d, budget):
     sorted_d = sorted(d)
     count = 0
-    
     for i in sorted_d:
         if budget < i:
             break
         budget -= i
         count += 1
-        
     return count

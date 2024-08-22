d123497cdcceadc6c57a4e98462e6fc99a640c98
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-22 14:52:38 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,7 +1,7 @@
 def solution(s):
-    number = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5",
-             "six":"6", "seven":"7", "eight":"8", "nine":"9"}
-    for i in number:
-        if i in s:
-            s = s.replace(i,number[i])
+    nums = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5",
+              "six":"6", "seven":"7", "eight":"8", "nine":"9", "zero": "0"}
+    for num in nums:
+        if num in s:
+            s = s.replace(num,nums[num])
     return int(s)

81df63fded34fe9cf59e369148b97d84818fc3cb
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-22 14:59:06 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,5 +1,5 @@
 def solution(food):
     answer = ""
-    for i,x in enumerate(food):
-        answer += str(i) * (x // 2)
+    for i,n in enumerate(food):
+        answer += str(i) * (n // 2)
     return answer + "0" + answer[::-1]

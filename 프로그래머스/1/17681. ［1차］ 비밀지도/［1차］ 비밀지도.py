be2734a39449a0e265124c4fe05801696ceb464e
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-23 16:05:16 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -3,6 +3,6 @@ def solution(n, arr1, arr2):
     for i in range(n):
         map1 = bin(arr1[i])[2:].zfill(n)
         map2 = bin(arr2[i])[2:].zfill(n)
-        line = ''.join('#' if map1[j] == '1' or map2[j] == '1' else ' ' for j in range(n))
+        line = ''.join('#' if int(map1[j]) + int(map2[j]) else ' ' for j in range(n))
         result.append(line)
     return result

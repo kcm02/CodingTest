845d90fde8864162e671a5680dffe96a8ed23cdb
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-27 13:34:56 +0900

    [level 1] Title: 추억 점수, Time: 1.06 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,12 +1,11 @@
 def solution(name, yearning, photo):
-    missing_score = {n: y for n, y in zip(name, yearning)}
+    score = {n:y for n,y in zip(name,yearning)}
     result = []
-    
-    for row in photo:
-        num = 0
-        for person in row:
-            if person in missing_score:
-                num += missing_score[person]
-        result.append(num)
-    
+    for persons in photo:
+        count = 0
+        for person in persons:
+            if person not in score:
+                continue
+            count += score[person]
+        result.append(count)
     return result

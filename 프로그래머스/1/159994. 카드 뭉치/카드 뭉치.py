20d0a5b625ddfe8fdb8e3ddefc36403b31dd619d
ChanMi Kim <112393082+kcm02@users.noreply.github.com>
2024-08-27 14:11:24 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,10 +1,10 @@
 def solution(cards1, cards2, goal):
     while goal:
         if cards1 and goal[0] == cards1[0]:
-            cards1.remove(cards1[0])
+            cards1.pop(0)
         elif cards2 and goal[0] == cards2[0]:
-            cards2.remove(cards2[0])
+            cards2.pop(0)
         else:
             return "No"
-        goal.remove(goal[0])
+        goal.pop(0)
     return "Yes"
